c0351c74a8d4632fba2cb1d856f7695d3258ba94
deepakmaan25 <dipumaan2002@gmail.com>
2026-03-29 01:45:56 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,8 @@
+import "@fontsource/aileron/300.css";
+import "@fontsource/aileron/400.css";
+import "@fontsource/aileron/600.css";
+import "@fontsource/aileron/700.css";
+import "@fontsource/aileron/400-italic.css";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
